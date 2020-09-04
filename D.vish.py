38046d6607ed080643291013befbf42c91e12f73
Deepansh Dubey <deepansh.dubey20@gmail.com>
2020-09-04 13:14:10 +0530

    Initial Commit

---
+++ D.vish.py
@@ -0,0 +1,127 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import smtplib
+import webbrowser as wb
+from PIL import Image
+
+wb.register('chrome',None)
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")
+
+    else:
+        speak("Good Evening!")
+
+    speak("Hi. I am Vish, How may I help you Sir!")
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        print("Say quit to exit.")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)
+        print("Say that again please...")
+        return "None"
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('youremail@gmail.com', 'your-password')
+    server.sendmail('youremail@gmail.com', to, content)
+    server.close()
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'show me the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Sir, the time is {strTime}")
+            print(strTime)
+            
+
+        elif 'open youtube' in query:
+            wb.open('https://www.youtube.com')
+
+        elif 'open google' in query:
+            wb.open('https://www.google.com')
+
+        elif 'open facebook' in query:
+            wb.open('https://www.facebook.com')
+
+        elif 'open coderg' in query:
+            wb.open("coderg.herokuapp.com")
+
+        elif 'quit' in query:
+            speak("Thanks for using this tiny software. Take care! Goodbye.")
+            exit()
+
+        elif 'play music' in query:
+            print("(Create a folder named as 'Music' in 'F' drive and put your music there.)")
+            music_dir = 'F:\\Music'
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+
+        elif 'play my favourite song' in query:
+            print("(Create a folder named as 'Fav' in 'F' drive and put your music there.)")
+            music_dir = 'F:\\Fav'
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+
+        elif 'email to Deepansh' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "deepansh.dubey20@gmail.com"
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry my friend. I am unable to send this email right now.")
